bb2bc47eb055d0f708e0ec09ad7b1102bee4de15
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-05-14 23:55:05 +0100

    Update file

--- client/src/pages/messages/Messages.jsx
+++ client/src/pages/messages/Messages.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-const Messages = () => {
+function Messages() {
+  const { id } = useParams();
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    fetchMessage(id);
+  }, [id]);
+
+  const fetchMessage = (id) => {
+   fetch(`https://mailler-backend.vercel.app/messages/${id}`)
+    .then(response => response.json())
+    .then(data => setMessage(data))
+    .catch(error => console.error('Error fetching message:', error));
+
+  };
+
   return (
-    <div>Messages</div>
-  )
+    <div className="message">
+      {message ? (
+        <div>
+          <h2 className='read_subject'>{message.subject}</h2>
+          <p>{message.content}</p>
+          {message.isRead ? <p>Message read</p> : <p>Unread</p>}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
 
-export default Messages
+export default Messages;
