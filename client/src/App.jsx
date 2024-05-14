fa24ae9b59a102386cb096cb7b5f0d0852e5f1e2
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-05-14 23:54:18 +0100

    Add path url for messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Routes>
           <Route path="/"  element={<Home/>} />
           <Route path="/inbox"  element={<Inbox/>} />
-          <Route path="/message/:id"  element={<Message/>} />
+          <Route path="/messages/:id"  element={<Message/>} />
     </Routes>
   );
 }
