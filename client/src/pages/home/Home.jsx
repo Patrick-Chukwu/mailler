b0e282625cbe097c30334b925b98857b5ed68a37
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-05-15 13:36:19 +0100

    Fetch total mails and unread mails Get number of mails from backend

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
 import './home.css'
+
 const Home = () => {
+  const [messages, setMessages ] = useState([])
+  const [unreadMessages, setUnreadMessages ] = useState(0)
     const user = "Pat";
-    const unreadMessage = 5;
-    const totalMessages = 20;
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://mailler-backend.vercel.app/messages');
+      const data = await response.json();
+      setMessages(data.messages);
+
+      // unread
+  const totalUnread = data.messages.filter(message => !message.isRead);
+  setUnreadMessages(totalUnread.length)
+    } catch (error) {
+      console.log("Error fetching number of unread messages", error)
+    }
+  }  
   return (
     <div className='column home'>
         <h2>Hello {user}</h2>
-        <p>You have {unreadMessage} unread messages out of {totalMessages} messages </p>
+        <p>You have <span className="unique">{unreadMessages}</span> unread mails out of <span className="unique">{messages.length}</span> total mails. </p>
         <Link className='button' to="/inbox">View messages</Link>
     </div>
   )
