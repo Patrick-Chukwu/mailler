103914bacc703a8d058c921cdcd12528d1b8a568
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-05-15 13:37:56 +0100

    Add containers to responses

--- client/src/pages/inbox/Inbox.jsx
+++ client/src/pages/inbox/Inbox.jsx
@@ -11,7 +11,7 @@ function Inbox() {
 
   const fetchMessages = async () => {
     try {
-      const response = await fetch('https://mailler-backend.vercel.app/api');
+      const response = await fetch('https://mailler-backend.vercel.app/messages');
       const data = await response.json();
       setMessages(data.messages);
     } catch (error) {
@@ -40,19 +40,37 @@ function Inbox() {
   return (
 
     <div className='inbox'>
-    <h2>Your Messages</h2>
+    <h2>Your Mails</h2>
     <ul>
       {messages.map((message, index) => (
         <li key={index}>
-          <h3 className='subject'>{message.subject}</h3>
+          <div className="column mail_container">
+          <div className="column mail">
+          <div className="row">
+            <h3>Subject: </h3>
+            <h3 className='subject'>{message.subject}</h3>
+          </div>
+
+          <div className="row">
+            <h3>Body: </h3>
+        
           <p>
             <Link className='links' to={`/messages/${index}`}
              onClick={() => updateReadStatus(index)}>
               {reduceContent(message.content, message.content.length / 2)}
             </Link>
           </p>
+          </div>
+          <div className="row mail_status">
+          <h3>Status: </h3>
           {message.isRead ? <span>Read</span> : <span>Unread mail</span>}
-        </li>
+  
+          </div>
+          </div>
+      
+          </div>
+         
+          </li>
       ))}
     </ul>
   </div>
