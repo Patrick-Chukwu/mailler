66a916ccc3056616f3ccee06dbd43a9fab948908
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2024-05-22 20:41:04 +0100

    feat: Update server

--- server/index.js
+++ server/index.js
@@ -39,5 +39,10 @@ app.put('/messages/mark-as-read/:index', (req, res) => {
   res.sendStatus(200);
 });
 
+// generate swagger UI
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json")
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(5000, () => { console.log("Server is running on Port 5000") });
